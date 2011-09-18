50c8d29d85caf8d9d17b7138010a64dab7c92b0d
Brian McFee <bmcfee@cs.ucsd.edu>
2011-09-18 16:13:20 -0700

    refactored the dispatcher, added search capability

---
+++ web/server/NodeSearch.py
@@ -0,0 +1,22 @@
+import whoosh, whoosh.index, whoosh.qparser
+import cjson as json
+import pprint
+
+class Root(object):
+
+    def __init__(self, indexPath):
+        self.index  = whoosh.index.open_dir(indexPath)
+        self.parser = whoosh.qparser.MultifieldParser(['title', 'artist', 'release', 'terms'], self.index.schema)
+
+    def search(self, querystring):
+        with self.index.searcher() as search:
+            results = search.search(self.parser.parse(unicode(querystring)), limit=10)
+            output = []
+            for r in results:
+                output.append({     'title':    r['title'],
+                                    'artist':   r['artist'],
+                                    'track_id': r['track_id'],
+                                    'release':  r['release'] })
+
+            return json.encode(output)
+        pass
