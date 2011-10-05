68d5c21e028dd784cd4a9966ee0526689b001250
Brian McFee <bmcfee@cs.ucsd.edu>
2011-10-04 19:27:01 -0700

    checking in whoosh indexer for msd names

---
+++ model/data/aotm/buildMSDtextIndex.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+'''
+CREATED:2011-10-04 19:16:19 by Brian McFee <bmcfee@cs.ucsd.edu>
+ 
+Index the MSD (artist, title) tuples with whoosh
+
+Usage:
+
+
+./buildMSDtextIndex.py /path/to/unique_tracks.py /path/to/whoosh/index
+
+'''
+
+import sys, os
+import cPickle as pickle
+import whoosh, whoosh.fields, whoosh.index, whoosh.analysis
+from whoosh.support.charset import accent_map
+
+
+def createIndexWriter(indexPath):
+    if not os.path.exists(indexPath):
+        os.mkdir(indexPath)
+
+    A = whoosh.analysis.StemmingAnalyzer() | whoosh.analysis.CharsetFilter(accent_map)
+
+    Schema = whoosh.fields.Schema(  song_id     = whoosh.fields.ID(stored=True),
+                                    artist      = whoosh.fields.TEXT(stored=True, analyzer=A),
+                                    title       = whoosh.fields.TEXT(stored=True, analyzer=A))
+
+    index = whoosh.index.create_in(indexPath, Schema)
+    return index.writer()
+    pass
+
+def parseTracks(trackfile):
+
+    with open(trackfile, 'r') as f:
+        for line in f:
+            (track, song, artist, title) = map(lambda x: unicode(x, 'latin-1', errors='replace'), line.strip().split('<SEP>', 4))
+            yield (song, artist, title)
+    pass
+
+def createIndex(trackfile, indexPath):
+        
+    writer = createIndexWriter(indexPath)
+
+    for (s, a, t) in parseTracks(trackfile):
+
+        writer.add_document(song_id     =   s,
+                            artist      =   a,
+                            title       =   t)
+    writer.commit()
+    pass
+
+if __name__ == '__main__':
+    createIndex(sys.argv[1], sys.argv[2])
+
