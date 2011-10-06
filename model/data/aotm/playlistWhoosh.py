541860e32577f4f1b36a43035f38171e6ac0bcb9
Brian McFee <bmcfee@cs.ucsd.edu>
2011-10-06 09:49:07 -0700

    removed plugins from playlist name resolver

--- model/data/aotm/playlistWhoosh.py
+++ model/data/aotm/playlistWhoosh.py
@@ -10,7 +10,7 @@ Usage:
 ./playlistWhoosh.py playlist_pickle /path/to/text_index filtered_playlists.pickle
 '''
 
-import cPickle as pickle, json
+import cPickle as pickle
 import sys, os, glob
 import pprint
 
@@ -33,11 +33,15 @@ def filterThisPlaylist(P, searcher, qa, qt):
     # First, map the playlist to a sequence of song ids or nones
     songs = []
     for (artist, song) in P['playlist']:
+        if len(artist) == 0 or len(song) == 0:
+            continue
+#         pprint.pprint((artist, song))
         results = searcher.search(qa.parse(artist) & qt.parse(song), limit=1)
         if len(results) > 0:
             songs.append(results[0]['song_id'])
         else:
             songs.append(None)
+        pprint.pprint((artist, song, songs[-1]))
     # Now, split the sequence by Nones
     P['filtered_lists'] = [x for x in splitPlaylists(songs)]
     return P
@@ -56,6 +60,16 @@ def filterPlaylists(playlist_pickle, index_dir, filterpickle):
     with index.searcher() as searcher:
         qa = whoosh.qparser.QueryParser('artist', index.schema)
         qt = whoosh.qparser.QueryParser('title', index.schema)
+        
+        for x in [qa, qt]:
+            x.remove_plugin_class(whoosh.qparser.WildcardPlugin)
+            x.remove_plugin_class(whoosh.qparser.PrefixPlugin)
+            x.remove_plugin_class(whoosh.qparser.RegexPlugin)
+            x.remove_plugin_class(whoosh.qparser.BoostPlugin)
+            x.remove_plugin_class(whoosh.qparser.OperatorsPlugin)
+            x.remove_plugin_class(whoosh.qparser.FieldsPlugin)
+            x.remove_plugin_class(whoosh.qparser.PlusMinusPlugin)
+
         for (i, P) in enumerate(playlists):
             filtered_playlists.append(filterThisPlaylist(P, searcher, qa, qt))
             if i % 10 == 0:
