2e59873b2b3213999776419291ca70d37bc60e10
Brian McFee <bmcfee@cs.ucsd.edu>
2011-11-02 16:51:34 -0700

    featuremap implementation for binary tag vectors

--- model/code/taghopper/__init__.py
+++ model/code/taghopper/__init__.py
@@ -2,39 +2,23 @@
 
 import numpy
 
-class tagVector(set):
+class TagVector(set):
     def __init__(self, a):
-        self.__index = set()
-        if isinstance(a, set):
-            self.__index.update(a)
-        else:
-            raise TypeError("Input object must be of type: set")
+        super(TagVector, self).__init__(a)
         pass
 
-    def getTags(self):
-        return self.__index
-
-    def __len__(self):
-        return len(self.__index)
-
-    def __contains__(self, val):
-        return val in self.__index
-
-    def __iter__(self):
-        return self.__index.__iter__()
-
     def __repr__(self):
-        return 'tagVector(%s)' % self.__index.__repr__()
+        return super(TagVector, self).__repr__()
 
     def __str__(self):
-        return 'tagVector(%s)' % self.__index.__str__()
+        return super(TagVector, self).__str__()
 
     def __and__(self, b):
-        if isinstance(b, tagVector):
-            return tagVector(self.__index & b.getTags())
+        if isinstance(b, TagVector):
+            return TagVector(set(self) & set(b))
         elif isinstance(b, numpy.ndarray):
             c = numpy.zeros(b.shape)
-            for i in self.__index:
+            for i in self:
                 c[i] = b[i]
             return c
         raise TypeError("Argument type not supported: %s" % type(b))
@@ -44,4 +28,34 @@ class tagVector(set):
         return self.__and__(b)
         pass
 
+#---#
 
+class FeatureMap(dict):
+    def __init__(self, vocab):
+        self.__vocabulary   = vocab
+        self.__vocabIndex   = {}
+        for (i,v) in enumerate(self.__vocabulary):
+            self.__vocabIndex[v] = i
+            pass
+        pass
+
+    def __setitem__(self, key, value):
+        idx = [self.__vocabIndex[v] for v in value]
+        super(FeatureMap, self).__setitem__(key, TagVector(v))
+        pass
+
+    def clear(self):
+        del self.__vocabulary
+        super(FeatureMap, self).clear()
+        pass
+
+    def getVocab(self):
+        return self.__vocabulary
+
+    def tagItem(self, x):
+        return [self.__vocabulary[i] for i in x]
+
+    def dimension(self):
+        return len(self.__vocabulary)
+
+#---#
